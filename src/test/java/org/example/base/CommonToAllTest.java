1de45940b9e539c9a912f5fa82dc999f3c4bb726
Yashaswini200 <yashusuresh2050@gmail.com>
2025-07-08 20:13:43 +0530

    commit

---
+++ src/test/java/org/example/base/CommonToAllTest.java
@@ -0,0 +1,18 @@
+package org.example.base;
+
+import org.example.Driver.DriverManager;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import static org.example.Driver.DriverManager.driver;
+
+public class CommonToAllTest {
+    @BeforeMethod
+    public void start(){
+        DriverManager.init();
+    }
+    @AfterMethod
+    public void tear(){
+//DriverManager.down();
+    }
+}
